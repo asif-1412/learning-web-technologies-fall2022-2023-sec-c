25af76fa255324be5af230ead45617011d861239
asif-1412 <83407164+asif-1412@users.noreply.github.com>
2023-11-21 22:21:45 +0600

    Add files via upload

---
+++ lab6/gender.js
@@ -0,0 +1,20 @@
+function gendercheck()
+{
+    let male =document.getElementById('Male').checked;
+    let female=document.getElementById('Female').checked;
+    let other =document.getElementById('Other').checked;
+
+    if(male)
+    {
+        document.getElementById("id3").innerHTML="Male";
+    }
+    else if(female)
+    {
+        document.getElementById("id3").innerHTML="Female";
+    }
+    else{
+         alert("You must select at least one gender!");
+    }
+
+
+}
